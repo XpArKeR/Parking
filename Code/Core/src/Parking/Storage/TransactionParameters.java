a5f80adf7875bf1b977cdd6c45d6ec0e8b3812f1
XpArKeR <xparker@gmx.net>
2018-06-01 20:35:52 +0200

    Added TransactionParameter class to the MySQL Loader. Will have to add to the core later.

---
+++ Code/Core/src/Parking/Storage/TransactionParameters.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Parking.Storage;
+
+/**
+ *
+ * @author noldi
+ */
+public class TransactionParameters 
+{
+    public TransactionParameters()
+    {
+        
+    }
+    
+    public Boolean IsSavingCascade = false;
+}
